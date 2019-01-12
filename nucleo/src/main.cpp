046dbf8ca1b30998d22c7ec034b3de758ea36e4f
Unknown <raddatzjonas@googlemail.com>
2019-01-12 17:15:13 +0100

    Testfunktion Ethernet rauswerfen

--- nucleo/src/main.cpp
+++ nucleo/src/main.cpp
@@ -63,38 +63,6 @@ void scanI2c(I2C &ic)
   }
 }
 
-void ethernetEchoExample(){
-  EthernetInterface net;
-  // Bring up the ethernet interface
-  printf("Ethernet socket example\n");
-  net.connect();
-
-  // Show the network address
-  const char *ip = net.get_ip_address();
-  printf("IP address is: %s\n", ip ? ip : "No IP");
-
-  // Open a socket on the network interface, and create a TCP connection to mbed.org
-  TCPSocket socket;
-  socket.open(&net);
-  socket.connect(constants::ECHO_SERVER_ADDRESS, constants::ECHO_SERVER_PORT);
-
-  // Send a simple http request
-  char sbuffer[] = "GET / HTTP/1.1\r\nHost: www.arm.com\r\n\r\n";
-  int scount = socket.send(sbuffer, sizeof sbuffer);
-  printf("sent %d [%.*s]\n", scount, strstr(sbuffer, "\r\n")-sbuffer, sbuffer);
-
-
-  // Recieve a simple http response and print out the response line
-  char rbuffer[64];
-  int rcount = socket.recv(rbuffer, sizeof rbuffer);
-  pc.printf("recv %d [%.*s]\n", rcount, strstr(rbuffer, "\r\n")-rbuffer, rbuffer);
-  // Close the socket to return its memory and bring down the network interface
-  socket.close();
-  // Bring down the ethernet interface
-  net.disconnect();
-  pc.printf("Done\n");
-}
-
 void resetI2C(){
   notReset = 0;
   wait_ms(1);
